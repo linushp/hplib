a8b02230c2eaa86381f5fbbdb1174bac15aa6442
luanhaipeng <luanhaipeng@staff.beechat.io>
2020-11-27 17:48:09 +0800

    prefix classname

---
+++ prefix-classname/demo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {getPrefixClassNames} from './prefix-classname';
+
+const classNames = getPrefixClassNames('abc');
+
+
+const App = ()=>{
+    return (
+        <div className={classNames('root')}>
+            实际使用的className为：abc-root
+        </div>
+    );
+}
---
+++ prefix-classname/prefix-classname.d.ts
@@ -0,0 +1,1 @@
+export declare function getPrefixClassNames(prefixCls: string): (names: string | string[], isWithPrefix?: boolean) => string;
