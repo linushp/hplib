5ef0dca3c2ca82cdadb184ef284aa7dd41ba3572
hellomaokai <910010653@qq.com>
2025-05-05 13:00:24 +0800

    Update types.d.ts

--- packages/aes-128-cbc/types.d.ts
+++ packages/aes-128-cbc/types.d.ts
@@ -1,9 +1,14 @@
-export type ArrayBufferType = ArrayBuffer | Buffer;
 
 export declare class Aes128CBC {
-  public constructor(keyHexStr: string, ivStr: string);
+  public constructor(keyHexStr?: string, ivStr?: string);
 
-  public encrypt(data: ArrayBufferType): Promise<ArrayBufferType>;
+  public encrypt(data: ArrayBuffer): Promise<ArrayBuffer>;
+  public decrypt(data: ArrayBuffer): Promise<ArrayBuffer>;
 
-  public decrypt(data: ArrayBufferType): Promise<ArrayBufferType>;
+  public encrypt_utf8_base64(data: string): Promise<string>;
+  public decrypt_base64_utf8(data: string): Promise<string>;
+
+  public time_encrypt_utf8_base64(data: string): Promise<string>;
+  public time_decrypt_base64_utf8(data: string): Promise<string>;
+
 }
