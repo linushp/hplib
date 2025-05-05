0d977dd48e5408c9f155c3e0f6e921c3eb749f24
hellomaokai <910010653@qq.com>
2025-05-05 12:59:58 +0800

    Update web-aes-utils.js

--- packages/aes-128-cbc/libs/web-aes-utils.js
+++ packages/aes-128-cbc/libs/web-aes-utils.js
@@ -10,41 +10,84 @@ function hex2Uint8Array(hexStr) {
   return arr;
 }
 
+function arrayBufferToBase64(arrayBuffer) {
+  const uint8Array = new Uint8Array(arrayBuffer);
+  let binary = '';
+  for (let i = 0; i < uint8Array.length; i++) {
+    binary += String.fromCharCode(uint8Array[i]);
+  }
+  return btoa(binary);
+}
+
+function base64ToArrayBuffer(base64) {
+  const binaryString = atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
 class Aes128CBC {
   constructor(keyHexStr, ivStr) {
-    this._key = hex2Uint8Array(keyHexStr || "e3f9d86f1c2e834ef5bd7f25f51da093");
-    this._cryptoKeyPromise = this.importSecretKey(this._key);
-    this._iv = hex2Uint8Array(ivStr || "808ce75f6ea37c071fe8075887717594");
+    this._key = hex2Uint8Array(keyHexStr || 'fc3cef4b7676028bc201cdc3e80e0f5f');
+    this._iv = hex2Uint8Array(ivStr || 'aa4981fa858f0a51b45c410d623222a4');
+    this._algorithm = { name: 'AES-CBC', iv: this._iv };
   }
 
-  importSecretKey(rawKey) {
-    return window.crypto.subtle.importKey("raw", rawKey, "AES-CBC", true, ["encrypt", "decrypt"]);
+  async importKey() {
+    if (!this._importKey_) {
+      this._importKey_ = await window.crypto.subtle.importKey(
+        'raw',
+        this._key,
+        this._algorithm.name,
+        false,
+        ['encrypt', 'decrypt']
+      );
+    }
+    return this._importKey_;
   }
 
-  async encrypt(data) {
-    const cryptoKey = await this._cryptoKeyPromise;
-    return window.crypto.subtle.encrypt(
-      {
-        iv: this._iv,
-        name: "AES-CBC"
-      },
-      cryptoKey,
-      data
-    );
+  async encrypt(arrayBuffer) {
+    const key = await this.importKey();
+    return window.crypto.subtle.encrypt(this._algorithm, key, arrayBuffer);
   }
 
-  async decrypt(data) {
-    const cryptoKey = await this._cryptoKeyPromise;
-    return window.crypto.subtle.decrypt(
-      {
-        iv: this._iv,
-        name: "AES-CBC"
-      },
-      cryptoKey,
-      data
-    );
+  async decrypt(encryptedDataArrayBuffer) {
+    const key = await this.importKey();
+    return window.crypto.subtle.decrypt(this._algorithm, key, encryptedDataArrayBuffer);
+  }
+
+  async encrypt_utf8_base64(utf8Str) {
+    const inputBuffer = new TextEncoder().encode(utf8Str);
+    const outputBuffer = await this.encrypt(inputBuffer);
+    return arrayBufferToBase64(outputBuffer);
+  }
+
+  async decrypt_base64_utf8(base64) {
+    const inputBuffer = base64ToArrayBuffer(base64);
+    const outputBuffer = await this.decrypt(inputBuffer);
+    return new TextDecoder().decode(new Uint8Array(outputBuffer));
+  }
+
+  async time_encrypt_utf8_base64(utf8Str) {
+    const c = Math.floor(Math.random() * 1000);
+    const t = Math.floor(Math.random() * 1000);
+    const json = JSON.stringify({ t, s: utf8Str, c });
+    return this.encrypt_utf8_base64(json);
+  }
+
+  async time_decrypt_base64_utf8(base64) {
+    const json = await this.decrypt_base64_utf8(base64);
+    const obj = JSON.parse(json);
+    if (!obj.s) {
+      throw new Error('time_decrypt_base64_utf8解析失败');
+    }
+    return obj.s;
   }
 }
+
 
 if (typeof module !== "undefined") {
   module.exports = {
